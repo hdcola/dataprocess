aa2965513de9c345574415f27acd309dd8ce8b05
JunJian <junjian@mochui.net>
2015-09-23 21:51:06 +0800

    拆分脚本实现时间段控制，并修改为管道输出

--- vvc/split_kafka.py
+++ vvc/split_kafka.py
@@ -5,41 +5,43 @@ import fileinput
 import sys
 import time
 import string
-import gzip
 
+filesfps = []
+filesfpscount = 0
+def testTime(timetmp, start_time, end_time):
+    try:
+        timedata = time.strptime(timetmp, "[%d/%b/%Y:%H:%M:%S+0800]")
+        timeStamp = int(time.mktime(timedata))
+    except ValueError :
+        raise ValueError("timeerr")
+    if (timeStamp >= int(start_time) and timeStamp <= int(end_time)):
+        return True
+    else:
+        return False
 
-def formatTime(timetmp):
-    timedata = time.strptime(timetmp, "[%d/%b/%Y:%H:%M:%S+0800]")
-    tm_min = timedata.tm_min / 5 * 5 + 5
-    tm_hour = timedata.tm_hour
-    if tm_min == 60:
-        tm_min = '00'
-        tm_hour = timedata.tm_hour + 1
-    if tm_min < 10:
-        tm_min = '0'+str(tm_min)
-    if tm_hour < 10:
-        tm_hour = '0'+str(tm_hour)
-    timetmp = time.strftime('%Y%m%d', timedata)+str(tm_hour)+str(tm_min)
-    return timetmp
-
-def split_kafka(line, pipe_type):
-    record = string.split(line, ' ')
-    timetmp = str(record[3]) + str(record[4])
-    timetmp = formatTime(timetmp)
-    filename = str(pipe_type) + "_" + str(timetmp)
-    fp = open(filename, "a")
-    fp.write(line)
-    fp.close()
+def split_kafka(line, start_time, end_time):
+    record = string.split(line, '- -')
+    record = record[1].strip().split(' ')
+    timetmp = str(record[0]) + str(record[1])
+    try:
+        if testTime(timetmp, start_time, end_time):
+            print line
+    except ValueError:
+        sys.stderr.write(("timeerr,%s") % line)
 
 
 if __name__ == '__main__':
     # gzcat abc.gz | python split_kafka.py "mpp_vv_pcweb" -
     # python split_kafka.py "mpp_vv_pcweb" afile bfile cfile
-    try:
-        pipe_type = sys.argv[1]
-        for line in fileinput.input(sys.argv[2:]):
-            split_kafka(line, pipe_type)
-    except IndexError as e:
-        pipe_type = "mpp_vv_pcweb"
-        for line in fileinput.input(sys.argv[1:]):
-            split_kafka(line, pipe_type)
+    start_time = sys.argv[1]
+    if len(start_time) != 10:
+        start_time = str(start_time) + "0"*(12-len(start_time))
+    timedata = time.strptime(start_time, "%Y%m%d%H%M")
+    start_time = int(time.mktime(timedata))
+    end_time = sys.argv[2]
+    if len(end_time) != 10:
+        end_time = str(end_time) + "0"*(12-len(end_time))
+    timedata = time.strptime(end_time, "%Y%m%d%H%M")
+    end_time = int(time.mktime(timedata))
+    for line in fileinput.input(sys.argv[3:]):
+        split_kafka(line. start_time, end_time)
