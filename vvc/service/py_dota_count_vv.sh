09f70c5602b62a510501cfcbe1283a9d351b8218
JunJian <junjian@mochui.net>
2015-09-30 04:06:49 +0800

    增加收集mpp_vv_pcclient

--- vvc/service/py_dota_count_vv.sh
+++ vvc/service/py_dota_count_vv.sh
@@ -8,18 +8,18 @@ sub_path_month=$4
 sub_path=${sub_path_year}/${sub_path_month}
 work_path="${pydota_path}"
 cd $work_path
-bzcat ${pydota_orig}/${sub_path}/$filename.bz2 | python bin/format/format_${topic}.py ./geoip \
+bzcat ${pydota_orig}/${sub_path}/$filename.bz2 | python format/format_${topic}.py ./geoip \
   2> ${pydota_des}/${sub_path}/err_${filename}.log | bzip2 > ${pydota_des}/${sub_path}/vv_${filename}.bz2
 
 if [ -f ${pydota_des}/${sub_path}/vv_${filename}.bz2 ]; then
-    python bin/count_vv_5min.py ${topic} "all" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
-    [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
+    #python bin/count_vv_5min.py ${topic} "all" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
+    #[ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
 
-    python bin/count_vv_5min.py ${topic} "chn" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
-    [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
+    #python bin/count_vv_5min.py ${topic} "chn" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
+    #[ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
 
-    python bin/count_vv_5min.py ${topic} "pl"  ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
-    [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
+    #python bin/count_vv_5min.py ${topic} "pl"  ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
+    #[ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
 
     python bin/count_vv_hour.py ${topic} "all" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
     [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
