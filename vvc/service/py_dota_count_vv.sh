452678f7a9288659a59833f1b7dc570e0e9e4407
老黄 <huangdong@gmail.com>
2015-10-06 22:51:27 +0800

    去除之前的bearychat的调用，准备开始使用新的bearychat.sh

--- vvc/service/py_dota_count_vv.sh
+++ vvc/service/py_dota_count_vv.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 . /etc/pydota.conf
 . $pydota_service/py_dota.common
 
@@ -14,15 +13,9 @@ work_path="${pydota_path}"
 cd $work_path
 filenameraw=${start_time}"_playrawdata_"${topic}
 
-srcno=`bzcat ${pydota_orig}/${sub_path}/$filename.bz2 | wc -l`
-bearchat_send "${0}开始${filenameraw}处理，${pydota_orig}/${sub_path}/$filename.bz2有${srcno}条记录"
 
 bzcat ${pydota_orig}/${sub_path}/$filename.bz2 | python format/format_${topic}.py ./geoip \
   2> ${pydota_des}/${sub_path}/err_${filenameraw}.log | bzip2 > ${pydota_des}/${sub_path}/${filenameraw}.bz2
-
-errno=`cat ${pydota_des}/${sub_path}/err_${filenameraw}.log| wc -l`
-srcno=`bzcat ${pydota_des}/${sub_path}/${filenameraw}.bz2 | wc -l`
-bearchat_send "${0}处理${filenameraw}完毕，${pydota_des}/${sub_path}/err_${filenameraw}.log:$errno，${pydota_des}/${sub_path}/${filenameraw}.bz2:${srcno}"
 
 if [ -f ${pydota_des}/${sub_path}/${filenameraw}.bz2 ]; then
     #python bin/count_vv_5min.py ${topic} "all" ${pydota_des}/${sub_path}/vv_$filename.bz2 ${pydota_report}/${sub_path} &
@@ -37,15 +30,9 @@ if [ -f ${pydota_des}/${sub_path}/${filenameraw}.bz2 ]; then
     python bin/count_vv_hour.py ${topic} "all" ${pydota_des}/${sub_path}/$filenameraw.bz2 ${pydota_report}/${sub_path} &
     [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
 
-    bearchat_send "count_vv_hour处理all的${pydota_des}/${sub_path}/$filenameraw.bz2完毕"
-
     python bin/count_vv_hour.py ${topic} "chn" ${pydota_des}/${sub_path}/$filenameraw.bz2 ${pydota_report}/${sub_path} &
     [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
 
-    bearchat_send "count_vv_hour处理chn的${pydota_des}/${sub_path}/$filenameraw.bz2完毕"
-
     python bin/count_vv_hour.py ${topic} "pl"  ${pydota_des}/${sub_path}/$filenameraw.bz2 ${pydota_report}/${sub_path} &
     [ ! -z ${pydota_process_pids} ] && echo $! >> ${pydota_process_pids}
-
-    bearchat_send "count_vv_hour处理pl的${pydota_des}/${sub_path}/$filenameraw.bz2完毕"
 fi
