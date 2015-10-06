fa9dc34b0407d710e7dfe48f5b03ee6f95115bd1
JunJian <junjian@mochui.net>
2015-10-06 21:17:36 +0800

    增加重算日志

--- vvc/service/py_dota_reprocess.sh
+++ vvc/service/py_dota_reprocess.sh
@@ -6,7 +6,7 @@
 count=$1
 n=0
 while [ $count -ge $n ]; do
-    hour=$((count-n))
+    hour=$((count-n+1))
     topics=("mpp_vv_pcweb mpp_vv_mobile mpp_vv_mobile_new_version mpp_vv_pcclient mpp_vv_msite mpp_vv_padweb mpp_vv_ott ott_vv_41 ott_vv_44")
     work_path="${pydota_path}"
     start_time=`date --date="$DATE - $hour hour" +%Y%m%d%H`
@@ -24,7 +24,7 @@ while [ $count -ge $n ]; do
     for topic in ${topics}; do
         filename=${start_time}"_play_"${topic}
         if [ -f ${pydota_orig}/${sub_path}/${filename}.bz2 ]; then
-          ./service/py_dota_count_vv_re.sh ${filename} $topic $sub_path_year $sub_path_month $start_time &
+            ./service/py_dota_count_vv_re.sh ${filename} $topic $sub_path_year $sub_path_month $start_time 2>${pydota_log}/${start_time}_${topic}
         fi
     done
     n=$((n+1))
