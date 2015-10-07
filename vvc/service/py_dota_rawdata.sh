c85d49c43b00eeb84d601d97fdbfbb1581d5dc75
老黄 <huangdong@gmail.com>
2015-10-08 01:07:08 +0800

    没有加入bearychat的定义，修复

--- vvc/service/py_dota_rawdata.sh
+++ vvc/service/py_dota_rawdata.sh
@@ -16,9 +16,15 @@ sub_path_month=${start_time:4:2}
 filename=${start_time}"_play_"${topic}
 sub_path=${sub_path_year}/${sub_path_month}
 work_path="${pydota_path}"
+bearychat="${work_path}/bin/bearychat.sh"
 
 cd $work_path
 filenameraw=${start_time}"_playrawdata_"${topic}
 
 bzcat ${pydota_orig}/${sub_path}/$filename.bz2 | python format/format_${topic}.py ./geoip \
   2> ${pydota_des}/${sub_path}/err_${filenameraw}.log | bzip2 > ${pydota_des}/${sub_path}/${filenameraw}.bz2
+
+err_count=`cat ${pydota_des}/${sub_path}/err_${filenameraw}.log | wc -l`
+raw_size=`ls -l ${pydota_des}/${sub_path}/${filenameraw}.bz2 | awk '{print $5}'`
+msg="error数据有${err_count}条,raw数据有${raw_size}"
+echo "${msg}" | $bearychat -t "${topic} ${start_time}的rawdata数据处理完成"
