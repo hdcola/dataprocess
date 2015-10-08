81eb5ab04a75a5b88f74bdfecfde3aec3eab8fc1
Gungdongfeng <fenqtsingyang@gmail.com>
2015-10-09 00:17:38 +0800

    加入 paste 命令解析

--- datacheck/datacheck.sh
+++ datacheck/datacheck.sh
@@ -5,20 +5,21 @@
 if [ "$1" ]; then
 	atime="$1"
 else
-	atime= $(date +%Y%m%d%H)
+	atime=$(date +%Y%m%d%H)
 
 junjian_path_orig="/home/junjian/pydota/orig/"
 junjian_path_des="/home/junjian/pydota/des/"
 junjian_path_report="/home/junjian/pydota/report/"
-junjian_path_sub="${atime:0:4}"
+ayear="${atime:0:4}"
+amonth="${atime:4:2}"
 #ben_path_orig="/data/matrix/"
 #ben_path_des=""
 ben_path_report="/data/marix/result/"
-ben_path_sub="${atime:0:8}"
+adate="${atime:0:8}"
 #echo ${junjian_path_report}
 #根据流判断 ben 的数据路径
 
-#topics=("mpp_vv_pcweb" "mpp_vv_mobile" "mpp_vv_mobile_new_version" "mpp_vv_pcclient" "mpp_vv_msite" "mpp_vv_padweb" "mpp_vv_ott" "ott_vv_41" "ott_vv_44")
+topics=("mpp_vv_pcweb" "mpp_vv_mobile" "mpp_vv_mobile_new_version" "mpp_vv_pcclient" "mpp_vv_msite" "mpp_vv_padweb" "mpp_vv_ott" "ott_vv_41" "ott_vv_44")
 #ben_path=("pcp mobile_old mobile pcc mz padweb ott ott41 ott44")
 
 #for topic in ${topics}; do
@@ -66,9 +67,14 @@ if [ "$2" ]; then
 	fi
 fi
 
-cat ${} 
+#获取 topics 的长度
+len_topics=${#tiopics[@]}
 
+#将各端 report 进行对比
+for ((i=0; i<${len_topics}; i++)); do
+	 paste "${junjian_path_report}${ayear}/${amonth}/${topics[$i]}_${adate}_vv_all_hour.csv" "${ben_path_report}${topics[$i]}.${adate}.hour.vv.csv" > /home/guangdong/datacheck/report/${topics[$i]}.${adate}.csv  
 
+done
 
 
 echo ${ben_path_des}
