a0e6629a327261261b9731f5040d14829cf744b1
Gungdongfeng <fenqtsingyang@gmail.com>
2015-10-11 11:27:30 +0800

    完善算法

--- datacheck/datacheck.sh
+++ datacheck/datacheck.sh
@@ -11,7 +11,7 @@ atime=2015100812
 
 junjian_path_orig="/home/junjian/pydota/orig/"
 junjian_path_des="/home/junjian/pydota/des/"
-
+datareport_path="/home/guangdong/datacheck/report/"
 #原始 report 路径
 #junjian_path_report="/home/junjian/pydota/report/"
 
@@ -26,64 +26,28 @@ adate="${atime:0:8}"
 #echo ${junjian_path_report}
 #根据流判断 ben 的数据路径
 
-echo "${atime}" "${amonth}" "${adate}"
+#echo "${atime}" "${amonth}" "${adate}"
 
 topics=("mpp_vv_pcweb" "mpp_vv_mobile" "mpp_vv_mobile_new_version" "mpp_vv_pcclient" "mpp_vv_msite" "mpp_vv_padweb" "mpp_vv_ott" "ott_vv_41" "ott_vv_44")
 ben_path=("pcp" "mobile_old" "mobile" "pcc" "mz" "padweb" "ott" "ott41" "ott44")
 
 #for topic in ${topics}; do
 #	if [ "$2" == "${topic}" ]; then
-		
 
 
-:>>COMMENT'
-if [ "$2" ]; then
-        if [ "$2" == "mpp_vv_pcweb" ]; then
-                ben_path_orig="/data/matrix/pcp/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/pcp/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/pcp/exception/"
-        elif [ "$2" == "mpp_vv_mobile" ]; then
-                ben_path_orig="/data/matrix/mobile_old/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/mobile_old/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/mobile_old/exception/"
-        elif [ "$2" == "mpp_vv_mobile_new_version" ]; then
-                ben_path_orig="/data/matrix/mobile/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/mobile/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/mobile/exception/"
-        elif [ "$2" == "mpp_vv_pcclient" ]; then
-                ben_path_orig="/data/matrix/pcc/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/pcc/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/pcc/exception/"
-        elif [ "$2" == "mpp_vv_msite" ]; then
-                ben_path_orig="/data/matrix/mz/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/mz/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/mz/exception/"
-        elif [ "$2" == "mpp_vv_padweb" ]; then
-                ben_path_orig="/data/matrix/padweb/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/padweb/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/padweb/exception/"
-        elif [ "$2" == "mpp_vv_ott" ]; then
-                ben_path_orig="/data/matrix/ott/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/ott/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/ott/exception/"
-        elif [ "$2" == "ott_vv_41" ]; then
-		ben_path_orig="/data/matrix/ott41/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/ott41/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/ott41/exception/"
-	elif [ "$2" == "ott_vv_44" ]; then
-		ben_path_orig="/data/matrix/ott44/raw/${ben_path_sub}/"
-                ben_path_des="/data/matrix/ott44/data/${ben_path_sub}/"
-                ben_path_err="/data/matrix/ott44/exception/"
-	fi
-fi
-COMMENT'
+#test chage
 #获取 topics 的长度
 len_topics=${#topics[@]}
 
 #将各端 report 进行对比
 for ((i=0; i<${len_topics}; i++)); do
-	 paste "${junjian_path_report}${ayear}/${amonth}/${topics[$i]}_${adate}_vv_all_hour.csv" "${ben_path_report}${ben_path[$i]}.${adate}.hour.vv.csv" | awk 'BEGIN {FS=","} {printf "%s,%s,%s\n", $1, $2, $4}' | awk 'BEGIN {FS=" "} {print $1","$3}' | awk 'BEGIN {FS=","} {printf "%s,%s,%s,%s,%s‰,%s\n", $1, $2, $3, $4, substr(($3-$4)/$4*1000,0,5), ($3-$4)}' > /home/guangdong/datacheck/report/${ben_path[$i]}.${adate}.csv    
 
+	echo "日期,军建时间,军建VV,ben时间,benVV,相差千分比,差值" > ${datareport_path}${topics[$i]}.${adate}.csv
+	#paste "${junjian_path_report}${ayear}/${amonth}/${topics[$i]}_${adate}_vv_all_hour.csv" "${ben_path_report}${ben_path[$i]}.${adate}.hour.vv.csv" > /home/guangdong/datacheck/report/${topics[$i]}.${adate}.test1.csv
+	 paste "${junjian_path_report}${ayear}/${amonth}/${topics[$i]}_${adate}_vv_all_hour.csv" "${ben_path_report}${ben_path[$i]}.${adate}.hour.vv.csv" | awk 'BEGIN {FS=","} {printf "%s,%s,%s\n", $1, $2, $4}' | \
+	 #计算
+	 awk 'BEGIN {FS=" "} {if ($3!="") {printf "%s,%s,%s\n", $1, substr($2,length($2)-5,2), $3} else {printf "%s,%s\n", $1, $2}}' | \
+	 awk 'BEGIN {FS=","} { if ($3!="" && $5!="") {printf "%s,%s,%s,%s,%s,%s‰,%s\n", $1, $2, $3, $4, $5, substr(($3-$5)/$5*1000, 0, 5), ($3-$5)} \
+   else if (length($1)<10) {printf "%s,%s,%s,null,null,null,null\n", $1, $2, $3 } \
+   else {printf "null,null,null,%s,%s,null,null\n", substr($1,length($1)-5,2), $2}}' >> ${datareport_path}${topics[$i]}.${adate}.csv
 done
-
-
