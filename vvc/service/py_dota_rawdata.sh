afda5e0b98a768a231e6cee371d53fb6bba5400b
老黄 <huangdong@gmail.com>
2015-10-12 01:07:39 +0800

    加入report vv的功能
    在计算完raw data后，自动开始计算paid cid vid的统计vv csv，并将计算结果发送到bearychat

--- vvc/service/py_dota_rawdata.sh
+++ vvc/service/py_dota_rawdata.sh
@@ -36,3 +36,6 @@ nowtime=`date "+%Y/%m/%d %H:%M:%S"`
 msg="${msg}
 ${proctime}-${nowtime}@${py_dota_process_user}"
 echo "${msg}" | $bearychat -t "${topic} ${start_time}的rawdata数据处理完成"
+
+# 接下来处理这个topic的vv report
+./service/py_dota_report_vv.sh $start_time $topic &
