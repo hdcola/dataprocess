451854a463c43e93c7179a9444da6a67073c8d7d
老黄 <huangdong@gmail.com>
2015-10-13 09:05:50 +0800

    加入小时、天、月数据统计
    小时数据是每个类型的细分当小时数据，天是当天小时数据汇总，月是当月每天的数据汇总

--- vvc/service/py_dota_report_vv_hour.sh
+++ vvc/service/py_dota_report_vv_hour.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# 计算指定天的vv数据
-# py_dota_report_vv_day.sh 20151007
-# 使用指定维度计算指定天的vv数据
-# py_dota_report_vv_day.sh 2015100722 cid
-
+# 计算指定小时的vv指定维度数据
+# py_dota_report_vv_hour.sh 2015100722 cid
+# 计算指定小时的vv所有维度数据
+# py_dota_report_vv_hour.sh 2015100722
 
 if [[ -e "/etc/pydota.conf" ]]; then
   . /etc/pydota.conf
@@ -27,7 +26,7 @@ function report(){
   filename=${start_time}*${field}.csv
   files=`ls ${pydota_report}${sub_path}/${filename}`
   cat ${files} | awk -F, '{
-    type=$1","$2;
+    type=$1","$2","$3;
     if(!(type in sum)){
       sum[type]=0};
       sum[type]=sum[type]+$NF
@@ -36,14 +35,18 @@ function report(){
       for(i in sum) {
         print sum[i]" "i
       }
-    }' | awk '{print $2","$1}' | sort \
-    > ${pydota_report}/${sub_path}/day_vv_${start_time}.csv
+    }'  | sort -rn | awk '{print $2","$1}' \
+    > ${pydota_report}/${sub_path}/hour_vv_${field}_${start_time}.csv
 }
 
 if [ -n "$2" ]; then
   field=$2
   report
 else
+  field="plid"
+  report
+  field="vid"
+  report
   field="cid"
   report
 fi
--- vvc/service/py_dota_report_vv_month.sh
+++ vvc/service/py_dota_report_vv_month.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# 计算指定天的vv数据
-# py_dota_report_vv_day.sh 20151007
-# 使用指定维度计算指定天的vv数据
-# py_dota_report_vv_day.sh 2015100722 cid
-
+# 计算指定月的vv数据
+# py_dota_report_vv_month.sh 201510
 
 if [[ -e "/etc/pydota.conf" ]]; then
   . /etc/pydota.conf
@@ -14,20 +11,18 @@ if [[ -n "$HOME" && -e "$HOME/.pydota" ]]; then
 fi
 
 start_time=$1
-field=$2
 sub_path_year=${start_time:0:4}
 sub_path_month=${start_time:4:2}
-sub_path_day=${start_time:6:2}
 sub_path=${sub_path_year}/${sub_path_month}
 work_path="${pydota_path}"
 bearychat="${work_path}/bin/bearychat.sh"
 cd $work_path
 
 function report(){
-  filename=${start_time}*${field}.csv
+  filename=day_vv_${start_time}*.csv
   files=`ls ${pydota_report}${sub_path}/${filename}`
   cat ${files} | awk -F, '{
-    type=$1","$2;
+    type=$1;
     if(!(type in sum)){
       sum[type]=0};
       sum[type]=sum[type]+$NF
@@ -37,13 +32,7 @@ function report(){
         print sum[i]" "i
       }
     }' | awk '{print $2","$1}' | sort \
-    > ${pydota_report}/${sub_path}/day_vv_${start_time}.csv
+    > ${pydota_report}/${sub_path}/month_vv_${start_time}.csv
 }
 
-if [ -n "$2" ]; then
-  field=$2
-  report
-else
-  field="cid"
-  report
-fi
+report
